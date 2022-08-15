d9080fb1b08d9333814cb65e6198cbeba32c88dc
Brulibra <brulibra03@hotmail.com>
2022-08-15 12:06:25 -0300

    closing button at LoginModal, and state props

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,7 +1,35 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Modal Component</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">
+    Entre na sua Conta
+    </h1>
+
+    <button @click="close" class="text-4xl text-gray-600"> &times; </button>
+  </div>
 </template>
 <script>
-export default {};
+import useModal from "@/hooks/useModal";
+import { reactive } from "vue";
+export default {
+  setup() {
+    const modal = useModal()
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: "",
+        errorMessage: "",
+      },
+      password: {
+        value: "",
+        errorMessage: "",
+      },
+    });
+    return {
+      state,
+      close: modal.close
+    };
+  },
+};
 </script>
