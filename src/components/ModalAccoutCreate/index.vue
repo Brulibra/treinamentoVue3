8021db8a374a258fcb82adac4f644ce0741b62fa
Brulibra <brulibra03@hotmail.com>
2022-08-16 14:43:53 -0300

    HTML and DOM validations completed

--- src/components/ModalAccoutCreate/index.vue
+++ src/components/ModalAccoutCreate/index.vue
@@ -1,7 +1,198 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Modal Component</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">Crie uma Conta</h1>
+    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+      &times;
+    </button>
+  </div>
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Nome</span>
+        <input
+          v-model="state.name.value"
+          type="name"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          :class="{
+            'border-brand-danger': !!state.name.errorMessage,
+          }"
+          placeholder="your name"
+        />
+        <span
+          v-if="!!state.name.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.name.errorMessage }}
+        </span>
+      </label>
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          v-model="state.email.value"
+          type="email"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage,
+          }"
+          placeholder="your.email@email.com"
+        />
+        <span
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <input
+          v-model="state.password.value"
+          type="password"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          :class="{
+            'border-brand-danger': !!state.password.errorMessage,
+          }"
+          placeholder="your password"
+        />
+        <span
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Repita a Senha</span>
+        <input
+          v-model="state.repetPassword.value"
+          type="repetPassword"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          :class="{
+            'border-brand-danger': !!state.repetPassword.errorMessage,
+          }"
+          placeholder="repet your password"
+        />
+        <span
+          v-if="!!state.repetPassword.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.repetPassword.errorMessage }}
+        </span>
+      </label>
+      <button
+        :disabled="state.isLoading"
+        type="submit"
+        :class="{
+          'opacity-50': state.isLoading,
+        }"
+        class="px-8 py-3 mt-10 font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+      >
+        <icon v-if="state.isLoading" name="Loading" class="animate-spin" />
+        <span v-else>Registrar</span>
+      </button>
+    </form>
+  </div>
 </template>
 <script>
-export default {};
+import { reactive } from "vue";
+import { useField } from "vee-validate";
+import { useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
+
+import Icon from "../Icons/index.vue";
+import useModal from "@/hooks/useModal";
+import services from "@/sevices";
+
+import {
+  validateEmptyAndLength3,
+  validateEmptyAndEmail,
+} from "@/utils/validators";
+
+export default {
+  components: {
+    Icon,
+  },
+  setup() {
+    const modal = useModal();
+    const router = useRouter();
+    const toast = useToast();
+
+    const { value: nameValue, errorMessage: nameErrorMessage } = useField(
+      "name",
+      validateEmptyAndEmail
+    );
+    const { value: emailValue, errorMessage: emailErrorMessage } = useField(
+      "email",
+      validateEmptyAndEmail
+    );
+    const { value: passwordValue, errorMessage: passwordErrorMessage } =
+      useField("password", validateEmptyAndLength3);
+    const { value: repetPassword, errorMessage: repetPasswordErrorMessage } =
+      useField("password", validateEmptyAndLength3);
+
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      name: {
+        value: nameValue,
+        errorMessage: nameErrorMessage,
+      },
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage,
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage,
+      },
+      repetPassword: {
+        value: repetPassword,
+        errorMessage: repetPasswordErrorMessage,
+      },
+    });
+
+    async function handleSubmit() {
+      try {
+        toast.clear();
+        state.isLoading = true;
+        const { data, errors } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value,
+        });
+
+        if (!errors) {
+          window.localStorage.setItem("token", data.token);
+          router.push({ name: "Feedbacks" });
+          modal.close();
+          return;
+        }
+
+        if (errors.status === 404) {
+          toast.error("E-mail não encontrado");
+        }
+
+        if (errors.status === 401) {
+          toast.error("E-mail ou Senha inválido");
+        }
+
+        if (errors.status === 400) {
+          toast.error("Ocorreu um erro ao fazer o login");
+        }
+
+        state.isLoading = false;
+      } catch (error) {
+        state.isLoading = false;
+        state.hasErrors = !!error;
+        toast.error("Ocorreu um erro ao fazer o login");
+      }
+    }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit,
+    };
+  },
+};
 </script>
