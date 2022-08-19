74067bdb73a381574a2caafce04f8ad101d56a7a
Brulibra <brulibra03@hotmail.com>
2022-08-19 16:15:26 -0300

    icons and apiKey printing on screen

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -3,14 +3,69 @@
   <div class="flex justify-center w-full h-28 bg-brand-main">
     <HeaderLogged />
   </div>
+  <!-- <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <div v-if="!state.isLate.late">
+      {{ lateInvoice }}
+    </div>
+    <div v-else>
+      {{ openInvouce }}
+  </div>
+    </div> -->
   <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
     <h1 class="text-4xl font-black text center text-gray-800">Credenciais</h1>
     <p class="text-large text-center text-gray-800 font-regular">
       Guia de instalação e geração de suas credenciais
     </p>
   </div>
+  <div class="flex justify-center w-full h-full">
+    <div class="flex flex-col items-center w-4/5 max-w-6xl py-10">
+      <h1 class="text-3xl font-black text-brand-graydark">
+        Instalação e configuração
+      </h1>
+      <p class="mt-10 text-lg text-center text-gray-800 font-regular">
+        Esse aqui é a sua chave de api
+      </p>
+      <div class="flex py-3 pl-5 pr-20 mt-2 rounded items-center bg-brand-gray">
+        <span class="">{{ store.User.currentUser.apiKey }}</span>
+        <div class="flex ml-20 mr-5">
+          <icon name="copy" :color="_brandColors.graydark" size="24" class="cursor-pointer" />
+          <icon name="loading" :color="_brandColors.graydark" size="24" class="cursor-pointer ml-3" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import HeaderLogged from "@/components/HeaderLogged/index.vue";
-export default { components: { HeaderLogged } };
+import Icon from "@/components/Icons"
+import useStore from "@/hooks/useStore";
+import palette from "../../../palette";
+// import { reactive } from "vue";
+export default {
+  components: { HeaderLogged, Icon },
+
+  setup() {
+    const store = useStore();
+
+    // const lateInvoice = "Este é um boleto atrasado";
+    // const openInvouce = "Este é um boleto em aberto";
+    // const state = reactive({
+    //   isLate: false,
+    //   isOpen: false,
+    //   late: {
+    //     lateInvoice,
+    //   },
+    //   open: {
+    //     openInvouce,
+    //   },
+    // });
+    return {
+      store,
+      _brandColors:palette.brand,
+      // state,
+      // lateInvoice,
+      // openInvouce,
+    };
+  },
+};
 </script>
