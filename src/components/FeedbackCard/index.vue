b8a438009d99d1020b537ca627eca03aacd56750
Brulibra <brulibra03@hotmail.com>
2022-08-26 17:48:31 -0300

    (bug) feedbacard not rendering

---
+++ src/components/FeedbackCard/index.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div
+    class="flex flex-col px-8 py-6 rounded cursor-pointer bg-brand-gray"
+    @click="handleToggle"
+  >
+    <div class="flex items-center justify-between w-full mb-8">
+      <Badge :type="feedback.type" />
+      <span class="font-regular text-brand-graydark">
+        {{ feedback.createdAt }}
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+import Badge from "./Badge.vue";
+export default {
+  components: {
+    Badge,
+  },
+
+  props: { feedback: { type: Object, required: true } },
+  setup() {
+    function handleToggle() {}
+    return {
+      handleToggle,
+    };
+  },
+};
+</script>
