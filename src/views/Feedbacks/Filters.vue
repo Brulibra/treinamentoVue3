8308f5c1cbca87867ba5a0de13ef9c178943c03f
Brulibra <brulibra03@hotmail.com>
2022-08-26 15:46:11 -0300

    (bugfix)Filter list, displaying content

--- src/views/Feedbacks/Filters.vue
+++ src/views/Feedbacks/Filters.vue
@@ -5,44 +5,47 @@
 
     <ul class="flex flex-col mt-3 list-none">
       <li
-        class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
         v-for="filter in state.filters"
         :key="filter.label"
         :class="{ 'bg-gray-200 bg-opacity-50': filter.active }"
         @click="() => handleSelect(filter)"
+        class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
       >
         <div class="flex items-center">
           <span
-            :class="`'bg-${filter.color}`"
+            :class="filter.color.bg"
             class="inline-block w-2 h-2 mr-2 rounded-full"
           />
+          {{ filter.label }}
         </div>
         <span
           class="font-bold"
-          :class="filter.active ? `text-${filter.color}` : 'text-black'"
-          >{{ filter.amount }}</span
+          :class="filter.active ? filter.color.text : 'text-black'"
         >
+          {{ filter.amount }}
+        </span>
       </li>
     </ul>
   </div>
 </template>
+
 <script>
-import useStore from "@/hooks/useStore";
-import services from "@/services";
 import { reactive } from "vue";
+import services from "@/services";
+import useStore from "@/hooks/useStore";
 
 const LABELS = {
-  all: "TODOS",
+  all: "Todos",
   issue: "Problemas",
   idea: "Ideias",
   other: "Outros",
 };
 
 const COLORS = {
-  all: "brand-info",
-  issue: "brand-danger",
-  idea: "brand-warning",
-  other: "brand-graydark",
+  all: { text: "text-brand-info", bg: "bg-brand-info" },
+  issue: { text: "text-brand-danger", bg: "bg-brand-danger" },
+  idea: { text: "text-brand-warning", bg: "bg-brand-warning" },
+  other: { text: "text-brand-graydark", bg: "bg-brand-graydark" },
 };
 
 function applyFiltersStructure(summary) {
@@ -60,14 +63,13 @@ function applyFiltersStructure(summary) {
     }
 
     return [...acc, currentFilter];
-  });
+  }, []);
 }
 
 export default {
-  async setup(props, {emit}) {
+  async setup(_, { emit }) {
     const store = useStore("Global");
     const state = reactive({
-        isLoading: false,
       hasError: false,
       filters: [{ label: null, amount: null }],
     });
@@ -86,18 +88,18 @@ export default {
     }
 
     function handleSelect({ type }) {
-        if(store.isLoading){
-            return
+      if (store.isLoading) {
+        return;
+      }
+
+      state.filters = state.filters.map((filter) => {
+        if (filter.type === type) {
+          return { ...filter, active: true };
         }
+        return { ...filter, active: false };
+      });
 
-        state.filter = state.filters.map((filter) => {
-            if(filter.type === type){
-                return {...filter, active: true}
-            }
-            return{...filter, active: false}
-        })
-
-        emit('select', type)
+      emit("select", type);
     }
 
     return {
--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -3,6 +3,7 @@
   <div class="flex justify-center w-full h-28 bg-brand-main">
     <HeaderLogged />
   </div>
+
   <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
     <h1 class="text-4xl font-black text center text-gray-800">Feedbacks</h1>
     <p class="text-lg text-center text-gray-800 font-regular">
@@ -12,8 +13,8 @@
 
   <div class="flex justify-center w-full pb-20">
     <div class="w-4/5 max-w-6xl py-10 grid grid-col-4 gap-2">
-      <div class="text-3xl font-black">
-        <h1>Listagem</h1>
+      <div>
+        <h1 class="text-3xl font-black">Listagem</h1>
         <suspense>
           <template #default> <Filters /> </template>
           <template #fallback> loading ... </template>
