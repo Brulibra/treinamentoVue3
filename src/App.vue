9ed0635fc3404d93310d72d097a9e3e0f6b04aa5
Brulibra <brulibra03@hotmail.com>
2022-09-05 16:38:56 -0300

    refactor: Homepage css writing style

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ export default {
           }
           const { data } = await services.users.getMe();
           setCurrentUser(data)
-          console.log("data", data);
         }
       }
     );
--- src/views/Home/CustomHead.vue
+++ src/views/Home/CustomHead.vue
@@ -2,45 +2,30 @@
 <template>
   <header class="header">
     <div class="header-group">
-      <div class="flex items-center justify-between py-10">
+      <div class="header-login">
         <div class="w-28 lg:w-36">
-          <img
-            class="w-full"
-            src="../../assets/images/logo_white.png"
-            alt="logo"
-          />
+          <img class="w-full" src="../../assets/images/logo_white.png" alt="logo" />
         </div>
         <div class="flex">
-          <button
-          @click="() => emit('create-accout')"
-            class="px-6 py-2 font-bold rounded-full text-white focus:outline-none"
-          >
+          <button @click="() => emit('create-accout')" class="header-createAccoutButton">
             Crie uma Conta
           </button>
-          <button
-            @click="() => emit('login')"
-            class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none"
-          >
-            Entrar
-          </button>
+          <button @click="() => emit('login')" class="header-loginButton">Entrar</button>
         </div>
       </div>
-      <div class="flex flex-col mt-28">
-        <h1 class="text-4xl font-black text-white">
+      <div class="header-content">
+        <h1 class="header-mainText">
           Tenha um feedback <br />
           E faça seus clientes mais <br class="hidden lg:inline-block" />
           felizes!
         </h1>
-        <p class="text-lg font-medium text-white">
+        <p class="header-text">
           Receba ideias, reclamações e feedbacks com um
           <br class="hidden lg:inline-block" />
           simples widget na página.
         </p>
         <div>
-          <button
-          @click="() => emit('create-accout')"
-            class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none mt-10"
-          >
+          <button @click="() => emit('create-accout')" class="header-freeAccoutButton">
             Crie uma conta grátis
           </button>
         </div>
@@ -61,11 +46,46 @@ export default {
 
 <style scoped>
 .header {
-  @apply bg-brand-main w-full flex justify-center;
+  @apply flex justify-center;
+  @apply w-full;
+  @apply bg-brand-main;
   height: 700px;
 }
 .header-group {
-  @apply flex flex-col w-4/5 max-w-6xl;
+  @apply flex flex-col;
+  @apply w-4/5 max-w-6xl;
+}
+.header-login {
+  @apply flex items-center justify-between py-10;
+}
+.header-createAccoutButton {
+  @apply px-6 py-2;
+  @apply font-bold text-white;
+  @apply rounded-full;
+  @apply focus:outline-none;
+}
+.header-loginButton {
+  @apply px-6 py-2;
+  @apply font-bold text-brand-main;
+  @apply bg-white rounded-full;
+  @apply focus:outline-none;
+}
+.header-content {
+  @apply flex flex-col mt-28;
+}
+
+.header-mainText {
+  @apply text-4xl font-black text-white;
+}
+
+.header-text {
+  @apply text-lg font-medium text-white;
+}
+.header-freeAccoutButton {
+  @apply mt-10 px-6 py-2;
+  @apply font-bold  text-brand-main;
+  @apply bg-white rounded-full;
+  @apply focus:outline-none;
 }
 
 @media (min-width: 640px) {
