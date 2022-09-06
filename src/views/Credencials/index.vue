cebe485848f2c2a592a26d1e10f828fac160b56e
Brulibra <brulibra03@hotmail.com>
2022-09-06 14:00:28 -0300

    refactor: Credencials page css writing style

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -1,22 +1,20 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="flex justify-center w-full h-28 bg-brand-main">
+  <div class="credencials-menu">
     <HeaderLogged />
   </div>
 
-  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
-    <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
-    <p class="text-lg text-center text-gray-800 font-regular">
+  <div class="credencials-header">
+    <h1 class="credencials-headerTitle">Credenciais</h1>
+    <p class="mt-10 credencials-headerText">
       Guia de instalação e geração de suas credenciais
     </p>
   </div>
 
-  <div class="flex justify-center w-full h-full">
-    <div class="flex flex-col w-4/5 max-w-6xl py-10">
-      <h1 class="text-3xl font-black text-black">Instalação e configuração</h1>
-      <p class="mt-10 text-lg text-gray-800 font-regular">
-        Esse aqui é a sua chave de api
-      </p>
+  <div class="credencials-main">
+    <div class="credencials-mainContent">
+      <h1 class="credencials-mainTitle">Instalação e configuração</h1>
+      <p class="mt-10 credencials-mainText">Esse aqui é a sua chave de api</p>
 
       <ContentLoader
         v-if="store.Global.isLoadin || state.isLoadin"
@@ -25,16 +23,13 @@
         height="50px"
       />
 
-      <div
-        v-else
-        class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
-      >
+      <div v-else class="credencials-mainApikey">
         <span v-if="state.hasErrors">Erro ao carregar a apiKey</span>
         <span v-else>{{ store.User.currentUser.apiKey }}</span>
 
-        <div v-if="!state.hasErrors" class="flex ml-20 mr-5">
+        <div v-if="!state.hasErrors" class="credencials-icons">
           <icon
-          @click="handleCopy"
+            @click="handleCopy"
             name="copy"
             :color="_brandColors.graydark"
             size="24"
@@ -50,7 +45,7 @@
         </div>
       </div>
 
-      <p class="mt-5 text-lg text-gray-800 font-regular">
+      <p class="mt-5 credencials-mainText">
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
       <ContentLoader
@@ -59,10 +54,7 @@
         width="600px"
         height="50px"
       />
-      <div
-        v-else
-        class="py-1 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
-      >
+      <div v-else class="credencials-mainScript">
         <span v-if="state.hasErrors">Erro ao carregar o script</span>
         <pre v-else>
 &lt;script src="http://Brulibra-feedbacker-widget.netlify.app?api_key={{
@@ -143,3 +135,38 @@ export default {
   },
 };
 </script>
+<style scoped>
+.credencials-menu {
+  @apply flex justify-center w-full h-28 bg-brand-main;
+}
+.credencials-header {
+  @apply flex flex-col items-center justify-center h-64 bg-brand-gray;
+}
+.credencials-headerTitle {
+  @apply text-4xl font-black text-center text-gray-800;
+}
+.credencials-headerText {
+  @apply text-lg text-center text-gray-800 font-regular;
+}
+.credencials-main {
+  @apply flex justify-center w-full h-full;
+}
+.credencials-mainContent {
+  @apply flex flex-col w-4/5 max-w-6xl py-10;
+}
+.credencials-mainTitle {
+  @apply text-3xl font-black text-black;
+}
+.credencials-mainText {
+  @apply text-lg text-gray-800 font-regular;
+}
+.credencials-mainApikey {
+  @apply flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2;
+}
+.credencials-icons {
+  @apply flex ml-20 mr-5;
+}
+.credencials-mainScript {
+  @apply py-1 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll;
+}
+</style>
